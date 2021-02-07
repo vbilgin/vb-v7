3ed0deef7d4e37988898415ed83a4de22a509a5f
Victor Bilgin <services@victorbilgin.com>
2021-02-07 17:40:26 +0000

    Testing dark mode improvement

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,39 +1,51 @@
-const toggleIcon = document.querySelector('.nav__color-toggle')
+const toggleIcon = document.querySelector(".nav__color-toggle");
 
-let currentColorScheme = localStorage.getItem('colorScheme')
+let currentColorScheme = localStorage.getItem("colorScheme");
 
 const setIconColor = (icon, colorScheme) => {
-    if (colorScheme === 'light') {
-        icon.setAttribute('fill', 'none')
-        icon.setAttribute('stroke', '#0000ff')
-    } else if (colorScheme === 'dark') {
-        toggleIcon.setAttribute('fill', '#F6AE2D')
-        toggleIcon.setAttribute('stroke', '#F6AE2D')
-    }
-}
+  if (colorScheme === "light") {
+    icon.setAttribute("fill", "none");
+    icon.setAttribute("stroke", "#0000ff");
+  } else if (colorScheme === "dark") {
+    toggleIcon.setAttribute("fill", "#F6AE2D");
+    toggleIcon.setAttribute("stroke", "#F6AE2D");
+  }
+};
+
+// const setInitialColorScheme = () => {
+//     if (document.documentElement.getAttribute('data-theme') !== currentColorScheme) {
+//         if (currentColorScheme === null || currentColorScheme === 'light') {
+//             document.documentElement.setAttribute('data-theme', 'light')
+//         } else if (currentColorScheme === 'dark') {
+//             document.documentElement.setAttribute('data-theme', 'dark')
+//             setIconColor(toggleIcon, 'dark')
+//         }
+//     }
+// }
 
 const setInitialColorScheme = () => {
-    if (document.documentElement.getAttribute('data-theme') !== currentColorScheme) {
-        if (currentColorScheme === null || currentColorScheme === 'light') {
-            document.documentElement.setAttribute('data-theme', 'light')
-        } else if (currentColorScheme === 'dark') {
-            document.documentElement.setAttribute('data-theme', 'dark')
-            setIconColor(toggleIcon, 'dark')
-        }
-    } 
-}
+  // if (
+  //   document.documentElement.getAttribute("data-theme") !== currentColorScheme
+  // ) {
+  if (currentColorScheme === "dark") {
+    document.documentElement.setAttribute("data-theme", "dark");
+    setIconColor(toggleIcon, "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+};
 
-setInitialColorScheme()
+setInitialColorScheme();
 
-toggleIcon.addEventListener('click', (e) => {
-    currentColorScheme = localStorage.getItem('colorScheme')
-    if (currentColorScheme === null || currentColorScheme === 'light') {
-        document.documentElement.setAttribute('data-theme', 'dark')
-        setIconColor(toggleIcon, 'dark')
-        localStorage.setItem('colorScheme', 'dark')
-    } else if (currentColorScheme === 'dark') {
-        document.documentElement.setAttribute('data-theme', 'light')
-        setIconColor(toggleIcon, 'light')
-        localStorage.setItem('colorScheme', 'light')
-    }
-})
+toggleIcon.addEventListener("click", () => {
+  currentColorScheme = localStorage.getItem("colorScheme");
+  if (currentColorScheme === null || currentColorScheme === "light") {
+    document.documentElement.setAttribute("data-theme", "dark");
+    setIconColor(toggleIcon, "dark");
+    localStorage.setItem("colorScheme", "dark");
+  } else if (currentColorScheme === "dark") {
+    document.documentElement.setAttribute("data-theme", "light");
+    setIconColor(toggleIcon, "light");
+    localStorage.setItem("colorScheme", "light");
+  }
+});
